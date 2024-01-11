c9a42c3c8c90d6838ccbe21753ed1551370e0c73
erickgodinho <godinho.erickc@gmail.com>
2024-01-10 23:40:44 -0300

    projeto terminado

---
+++ index.js
@@ -0,0 +1,52 @@
+//classificador de nivel de herói
+let nomeHeroi = "jorgin"
+let xpHeroi = "8001"
+
+console.log ("o herói de nome " + nomeHeroi + " está no nivel")
+switch (xpHeroi){
+    case "900":
+        console.log (" ferro");
+        break;
+
+    case "1001":
+        console.log (" bronze");
+        break;
+    
+    case " 2001":
+        console.log ("prata");
+        break;
+    
+    case "5001":
+        console.log ("ouro");
+        break
+    
+    case "7001":
+        console.log("platina");
+        break
+    
+    case "8001":
+        console.log("ascendente");
+        break
+    
+    case "9001":
+       console.log("imortal");
+       break
+    
+    case "10001":
+        console.log("radiante");
+        break
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
